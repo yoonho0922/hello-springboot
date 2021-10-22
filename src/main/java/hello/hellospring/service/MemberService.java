445ed7e88a461b65c8f6edf6102b01cec459af4d
Yoonho Ahn <yoonho5684@gmail.com>
2021-10-22 14:15:16 +0900

    feat: repository 의존성 주입 받을 수 있도록

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,8 +8,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member) {
         // 이름 중복 검사
