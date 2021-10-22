c0f9b94c0c3bfbe978ae8d5a8ef5c06ce86f91ad
Yoonho Ahn <yoonho5684@gmail.com>
2021-10-22 18:47:13 +0900

    feat: JdbcMemberRepository and h2 database

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,23 +1,32 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-    // 코드로 직접 스프링 빈 등록
+    private final DataSource dataSource;
 
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // 코드로 직접 스프링 빈 등록
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
+    public MemberRepository memberRepository() {
         // DB를 바꿀 경우 밑의 코드만 변경하면 됨 - 수동 스프링 빈 등록의 장점
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
