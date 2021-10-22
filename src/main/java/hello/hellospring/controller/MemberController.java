129152895db16e5d07427a782b2706eed97aec6f
Yoonho Ahn <yoonho5684@gmail.com>
2021-10-22 18:45:44 +0900

    feat: spring web

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,47 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+    /* MS 객체를 불필요하게 여러개 만들게 됨
+    private final MemberService memberService = new MemberService();
+    */
+
+    private MemberService memberService;
+
+    @Autowired // 스프링 컨테이너에서 객체를 가지고옴
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
